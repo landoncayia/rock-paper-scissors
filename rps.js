7e5de9fad314b72ef9c302c0cf42cb1821edc054
Landon Cayia <landon.cayia@gmail.com>
2022-08-29 12:24:41 -0400

    Create function for playing a single round

--- rps.js
+++ rps.js
@@ -3,3 +3,25 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return options[choice];
 }
+
+function playRound(playerSelection, computerSelection) {
+  // Normalize player input: first letter is uppercase, rest are lowercase
+  playerSelection = playerSelection[0].toUpperCase()
+    + playerSelection.substring(1);
+
+  if (playerSelection == "Rock" && computerSelection == "Paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    return "You Lose! Scissors beat Paper";
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return "You Win! Scissors beat Paper";
+  } else {
+    return "It's a Tie!";
+  }
+}
