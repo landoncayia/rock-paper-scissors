bb2d2ccb9ddc6d736ea31e3db2b2756374612445
Landon Cayia <landon.cayia@gmail.com>
2022-08-30 17:52:32 -0400

    Messages now show in results box

--- rps.js
+++ rps.js
@@ -5,6 +5,9 @@ let computerScore = 0;
 // "Holder" variable for the result of each round
 let roundResult;
 
+const optionButtons = document.querySelectorAll('.button-container > button');
+const results = document.querySelector('.results');
+
 function getComputerChoice() {
   const options = ["Rock", "Paper", "Scissors"]
   let choice = Math.floor(Math.random() * 3);
@@ -16,27 +19,35 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection[0].toUpperCase()
     + playerSelection.substring(1);
 
+  const msg = document.createElement('p');
+
   // Return -1 for player loss; 1 for player win; 0 for tie
   if (playerSelection == "Rock" && computerSelection == "Paper") {
-    console.log("You Lose! Paper beats Rock");
+    msg.textContent = "You Lose! Paper beats Rock";
+    results.appendChild(msg);
     return -1;
   } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    console.log("You Win! Rock beats Scissors");
+    msg.textContent = "You Win! Rock beats Scissors";
+    results.appendChild(msg);
     return 1;
   } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-    console.log("You Lose! Scissors beat Paper");
+    msg.textContent = "You Lose! Scissors beat Paper";
+    results.appendChild(msg);
     return -1;
   } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-    console.log("You Win! Paper beats Rock");
+    msg.textContent = "You Win! Paper beats Rock";
+    results.appendChild(msg);
     return 1;
   } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-    console.log("You Lose! Rock beats Scissors");
+    msg.textContent = "You Lose! Rock beats Scissors";
+    results.appendChild(msg);
     return -1;
   } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    console.log("You Win! Scissors beat Paper");
+    msg.textContent = "You Win! Scissors beat Paper";
     return 1;
   } else {
-    console.log("It's a Tie!");
+    msg.textContent = "It's a Tie!";
+    results.appendChild(msg);
     return 0;
   }
 }
@@ -48,8 +59,6 @@ function updateScores(result) {
     computerScore++;
   }
 }
-
-const optionButtons = document.querySelectorAll('.button-container > button');
 
 optionButtons.forEach((button) => {
   button.addEventListener('click', () => {
